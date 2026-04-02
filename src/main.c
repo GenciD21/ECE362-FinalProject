2f206d64e048b5e73d45dd5a0582d5d88bc2d850
GenciD21 <genci.bro@gmail.com>
2026-04-02 16:44:58 -0400

    test

---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+int main() {
+    // Configures our microcontroller to 
+    // communicate over UART through the TX/RX pins
+    stdio_init_all();    
+}
